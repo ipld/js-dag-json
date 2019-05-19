2ea8e3f0227f4e9df6da1042ee24bb24c9b2df18
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-05-19 20:17:26 +0000

    fix: browser testing

--- test/dag-json.spec.js
+++ test/dag-json.spec.js
@@ -1,7 +1,7 @@
 'use strict'
+/* globals it */
 const dag = require('../')
 const CID = require('cids')
-const { it } = require('mocha')
 const assert = require('assert')
 const tsame = require('tsame')
 
