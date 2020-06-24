c359012756aad50273cfc04298e3ba2d4f8cdc80
Mikeal Rogers <mikeal.rogers@gmail.com>
2020-06-24 13:32:59 -0700

    fix: upgrade file from globals repo

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,11 @@
-import path from 'path'
+import { readdirSync, readFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
 
-const configs = [{
-  input: 'index.js',
-  output: {
-    file: 'dist/index.cjs',
-    format: 'cjs'
-  }
-}]
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const pkg = JSON.parse(readFileSync(join(__dirname, 'package.json')))
 
 const relativeToMain = name => ({
   name: 'relative-to-main',
@@ -14,18 +13,22 @@ const relativeToMain = name => ({
     while (source.includes("require('../index.js')")) {
       source = source.replace("require('../index.js')", `require('${name}')`)
     }
+    while (source.includes("require('../')")) {
+      source = source.replace("require('../", `require('${name}/`)
+    }
     return source
   }
 })
 
-const plugins = [relativeToMain('@ipld/dag-cbor')]
-configs.push({
-  input: 'test/test-basics.js',
-  output: {
-    plugins,
-    file: path.join('dist', 'test', 'test-basics.cjs'),
-    format: 'cjs'
-  }
-})
+const plugins = [relativeToMain(pkg.name)]
+const dir = 'dist'
+const output = { dir, plugins, format: 'cjs', entryFileNames: '[name].cjs' }
+const testdir = join(__dirname, 'test')
+const filter = name => name.startsWith('test-')
+const createConfig = f => ({ input: join('test', f), output })
+
+const configs = readdirSync(testdir).filter(filter).map(createConfig)
+
+console.log(configs)
 
 export default configs
