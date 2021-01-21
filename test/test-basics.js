c550438aa7e902a53b289b81f211402430df2db0
Rod Vagg <rod@vagg.org>
2021-01-21 19:42:28 +1100

    feat!: switch to cborg & json backend, new strictness

--- test/test-basics.js
+++ test/test-basics.js
@@ -1,41 +1,39 @@
 'use strict'
-/* globals describe, it */
-import assert from 'assert'
+/* eslint-env mocha */
+import garbage from 'ipld-garbage'
+import chai from 'chai'
 import { encode, decode } from '@ipld/dag-json'
 import { bytes, CID } from 'multiformats'
 
+const { assert } = chai
 const same = assert.deepStrictEqual
 const test = it
 
-const recode = buffer => encode(decode(buffer))
+const recode = byts => encode(decode(byts))
 
 const link = CID.parse('bafyreifepiu23okq5zuyvyhsoiazv2icw2van3s7ko6d3ixl5jx2yj2yhu')
 
 describe('basic dag-json', () => {
   test('encode decode', () => {
-    let buffer = encode({ hello: 'world' })
-    same(JSON.parse(bytes.toString(recode(buffer))), { hello: 'world' })
-    const o = { link, buffer: bytes.fromString('asdf'), n: null, o: {} }
-    buffer = encode(o)
-    same(decode(buffer), o)
-    same(bytes.isBinary(decode(buffer).buffer), true)
-  })
-
-  test('circular failure', () => {
-    const o1 = { hello: 'world' }
-    const o2 = { o1 }
-    o1.o2 = o2
-    try {
-      encode(o2)
-      assert.ok(false)
-    } catch (e) {
-      same(e.message, 'Object contains circular references')
-    }
+    let byts = encode({ hello: 'world' })
+    same(JSON.parse(bytes.toString(recode(byts))), { hello: 'world' })
+    const o = { link, byts: bytes.fromString('asdf'), n: null, o: {} }
+    byts = encode(o)
+    same(decode(byts), o)
+    same(bytes.isBinary(decode(byts).byts), true)
   })
 
   test('use reserved space', () => {
-    const decoded = decode(encode({ '/': { type: 'stringName' } }))
-    same(decoded['/'].type, 'stringName')
+    // allowed
+    same(decode(encode({ '/': { bytes: true } })), { '/': { bytes: true } })
+    same(decode(encode({ '/': { type: 'stringName' } })), { '/': { type: 'stringName' } })
+    same(decode(encode({ '/': bytes.fromString('asdf') })), { '/': bytes.fromString('asdf') })
+
+    // TODO: test encode() doesn't allow this
+    assert.throws(() => decode(encode({ '/': link.toString(), bop: 'bip' })))
+    assert.throws(() => decode(encode({ '/': { bytes: 'mS7ldeA', bop: 'bip' } })))
+    assert.throws(() => decode(encode({ '/': { bytes: 'mS7ldeA' }, bop: 'bip' })))
+    assert.throws(() => decode(encode({ '/': { bytes: 'mS7ldeA', bop: 'bip' }, bop: 'bip' })))
   })
 
   test('native types', done => {
@@ -50,5 +48,71 @@ describe('basic dag-json', () => {
     same(flip([]), [])
     same(flip(['asdf']), ['asdf'])
     done()
+  })
+
+  test('error on circular references', () => {
+    const circularObj = {}
+    circularObj.a = circularObj
+    assert.throws(() => encode(circularObj), /object contains circular references/)
+    const circularArr = [circularObj]
+    circularObj.a = circularArr
+    assert.throws(() => encode(circularArr), /object contains circular references/)
+  })
+
+  test('error on encoding undefined', () => {
+    assert.throws(() => encode(undefined), /\Wundefined\W.*not supported/)
+    const objWithUndefined = { a: 'a', b: undefined }
+    assert.throws(() => encode(objWithUndefined), /\Wundefined\W.*not supported/)
+  })
+
+  test('error on encoding IEEE 754 specials', () => {
+    for (const special of [NaN, Infinity, -Infinity]) {
+      assert.throws(() => encode(special), new RegExp(`\\W${String(special)}\\W.*not supported`))
+      const objWithSpecial = { a: 'a', b: special }
+      assert.throws(() => encode(objWithSpecial), new RegExp(`\\W${String(special)}\\W.*not supported`))
+      const arrWithSpecial = [1, 1.1, -1, -1.1, Number.MAX_SAFE_INTEGER, special, Number.MIN_SAFE_INTEGER]
+      assert.throws(() => encode(arrWithSpecial), new RegExp(`\\W${String(special)}\\W.*not supported`))
+    }
+  })
+
+  test('fuzz serialize and deserialize with garbage', function () {
+    // filter out fuzz garbage for objects that are disqualified by DAG-JSON rules
+    const checkObj = (obj) => {
+      if (Array.isArray(obj)) {
+        return obj.every(checkObj)
+      }
+      if (obj && typeof obj === 'object') {
+        for (const [key, value] of Object.entries(obj)) {
+          if (key === '/') {
+            if (typeof value === 'string') {
+              return false
+            }
+            if (value && typeof value === 'object' && value.bytes !== undefined) {
+              return false
+            }
+          }
+          if (!checkObj(value)) {
+            return false
+          }
+        }
+      }
+      return true
+    }
+
+    this.timeout(5000)
+    for (let ii = 0; ii < 1000; ii++) {
+      const original = garbage(300)
+      if (!checkObj(original)) {
+        continue
+      }
+      try {
+        const encoded = encode(original)
+        const decoded = decode(encoded)
+        same(decoded, original)
+      } catch (err) {
+        console.log('Failed on fuzz object:', original)
+        throw err
+      }
+    }
   })
 })
