b288139ba8393d6e72e85017186f0f89deeb66ad
Rod Vagg <rod@vagg.org>
2025-05-22 14:27:00 +1000

    fix: address lint errors

--- test/ts-use/src/main.ts
+++ test/ts-use/src/main.ts
@@ -1,14 +1,15 @@
+/* eslint-disable no-console */
+
 import { deepStrictEqual } from 'assert'
-
-import { BlockEncoder, BlockDecoder, BlockCodec } from 'multiformats/codecs/interface'
 import * as dagJson from '@ipld/dag-json'
+import { BlockEncoder, BlockDecoder, BlockCodec } from 'multiformats/codecs/interface'
 
-const main = () => {
+const main = (): void => {
   // make sure we have a full CodecFeature
   useCodecFeature(dagJson)
 }
 
-function useCodecFeature (codec: BlockCodec<297, any>) {
+function useCodecFeature (codec: BlockCodec<297, any>): void {
   // use only as a BlockEncoder
   useEncoder(codec)
 
@@ -22,26 +23,26 @@ function useCodecFeature (codec: BlockCodec<297, any>) {
   useBlockCodec(codec)
 }
 
-function useEncoder<Codec extends number> (encoder: BlockEncoder<Codec, string>) {
+function useEncoder<Codec extends number> (encoder: BlockEncoder<Codec, string>): void {
   deepStrictEqual(encoder.code, 297)
   deepStrictEqual(encoder.name, 'dag-json')
   deepStrictEqual(Array.from(encoder.encode('blip')), [34, 98, 108, 105, 112, 34])
   console.log('[TS] ✓ { encoder: BlockEncoder }')
 }
 
-function useDecoder<Codec extends number> (decoder: BlockDecoder<Codec, Uint8Array>) {
+function useDecoder<Codec extends number> (decoder: BlockDecoder<Codec, Uint8Array>): void {
   deepStrictEqual(decoder.code, 297)
-  deepStrictEqual(decoder.decode(Uint8Array.from([34, 98, 108, 105, 112, 34 ])), 'blip')
+  deepStrictEqual(decoder.decode(Uint8Array.from([34, 98, 108, 105, 112, 34])), 'blip')
   console.log('[TS] ✓ { decoder: BlockDecoder }')
 }
 
-function useDecoderWithArrayBuffer<Codec extends number> (decoder: BlockDecoder<Codec, Uint8Array>) {
+function useDecoderWithArrayBuffer<Codec extends number> (decoder: BlockDecoder<Codec, Uint8Array>): void {
   deepStrictEqual(decoder.code, 297)
-  deepStrictEqual(decoder.decode(Uint8Array.from([34, 98, 108, 105, 112, 34 ]).buffer), 'blip')
+  deepStrictEqual(decoder.decode(Uint8Array.from([34, 98, 108, 105, 112, 34]).buffer), 'blip')
   console.log('[TS] ✓ { decoder: BlockDecoder }')
 }
 
-function useBlockCodec<Codec extends number> (blockCodec: BlockCodec<Codec, string>) {
+function useBlockCodec<Codec extends number> (blockCodec: BlockCodec<Codec, string>): void {
   deepStrictEqual(blockCodec.code, 297)
   deepStrictEqual(blockCodec.name, 'dag-json')
   deepStrictEqual(Array.from(blockCodec.encode('blip')), [34, 98, 108, 105, 112, 34])
